a797e47ea5f17df5cdab616a7325db0bed8cdac5
Chakradhar Chowdary <chakribathala@gmail.com>
2025-06-12 12:15:29 +0530

    Creating IStatusEffect

---
+++ Pokemon/include/Pokemon/IStatusEffect.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include<string>
+#include "../../include/Pokemon/Pokemon.hpp"
+
+using namespace std;
+namespace N_Pokemon {
+    class IStatusEffect {
+    public:
+        // Apply the effect (e.g., poison, burn)
+        virtual void applyEffect(Pokemon* target) = 0;
+
+        // Get the name of the effect
+        virtual string getEffectName() = 0;
+
+        // Apply the changes due to effect after the end of each turn
+        // Returns true if the Pokémon can move, else false
+        virtual bool turnEndEffect(Pokemon* target) = 0;
+
+        // Remove the effect
+        virtual void clearEffect(Pokemon* target) = 0;
+
+        virtual ~IStatusEffect() = default;
+    };
+}
